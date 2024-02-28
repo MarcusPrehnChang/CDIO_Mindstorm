a97b957acf747382c19f7956364d5c079a2532b3
Marcus Chang <marcuspc292@gmail.com>
2024-02-28 09:11:06 +0100

    first commit

---
+++ opencv_camera.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+
+def find_ball(frame):
+    print("find ball")
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5,5), 0)
+    edges = cv2.Canny(blurred, 50, 150)
+    contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    print(contours.count)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        perimeter = cv2.arcLength(contour, True)
+        circularity = 4 * np.pi * area / (perimeter * perimeter)
+
+        if circularity > 0.5:
+            ((x,y), radius) = cv2.minEnclosingCircle(contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+            cv2.circle(frame, center, radius, (0, 255, 255), 2)
+    return frame
+
+def main():
+    print("cockus")
+    #cap = cv2.VideoCapture(0)
+    #print("vidcap")
+    #if not cap.isOpened():
+        #print("Cannot open camera")
+        #exit()
+
+    input_image = cv2.imread('WIN_20240221_10_30_19_Pro.jpg')
+
+    if input_image is None:
+        print("Error: Could not open or read the image")
+        return
+    
+    output_image = find_ball(input_image)
+
+    cv2.imshow('Output Image', output_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+    #while True:
+        #print("in while")
+        #ret, frame = cap.read()
+        #frame_width = 640
+        #frame_height = 480
+        #cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+        #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+        #if not ret:
+            #print("Can't receive frame (stream end?). Exiting ...")
+            #break
+
+        #frame = find_ball(frame)
+        
+        # Display the resulting frame
+        #cv2.imshow('frame', frame)
+        #if cv2.waitKey(1) == ord('q'):
+            #break
+            
+
+    #cap.release()
+    #cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
