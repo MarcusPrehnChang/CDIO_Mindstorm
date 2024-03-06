de966727459985bc0af597532a79dcfe045dfd42
PH4MTOM <Simonmadsen99@gmail.com>
2024-03-06 09:04:56 +0100

    Added a max and min radius for detecting

--- opencv_camera.py
+++ opencv_camera.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 
-def find_ball(frame):
+
+def find_ball(frame, min_radius=20, max_radius=60):
     print("find ball")
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5,5), 0)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     edges = cv2.Canny(blurred, 50, 150)
     contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     print(contours.count)
@@ -17,51 +18,49 @@ def find_ball(frame):
         circularity = 4 * np.pi * area / (perimeter * perimeter)
 
         if circularity > 0.5:
-            ((x,y), radius) = cv2.minEnclosingCircle(contour)
+            ((x, y), radius) = cv2.minEnclosingCircle(contour)
             center = (int(x), int(y))
             radius = int(radius)
-            cv2.circle(frame, center, radius, (0, 255, 255), 2)
+            if min_radius < radius < max_radius:
+                cv2.circle(frame, center, radius, (0, 255, 255), 2)
     return frame
+
 
 def main():
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M','J','P','G'))
-
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
 
     print("vidcap")
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
 
-    #input_image = cv2.imread('WIN_20240221_10_30_19_Pro.jpg')
+    # input_image = cv2.imread('WIN_20240221_10_30_19_Pro.jpg')
 
-    #if input_image is None:
-        #print("Error: Could not open or read the image")
-        #return
-    
-    #output_image = find_ball(input_image)
+    # if input_image is None:
+    # print("Error: Could not open or read the image")
+    # return
 
-    #cv2.imshow('Output Image', output_image)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    # output_image = find_ball(input_image)
 
+    # cv2.imshow('Output Image', output_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     while True:
         print("in while")
         ret, frame = cap.read()
 
-
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-                
+
         frame = find_ball(frame)
-        
+
         # Display the resulting frame
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) == ord('q'):
             break
-            
 
     cap.release()
     cv2.destroyAllWindows()
