c487b50473a0c8518a9cbac293b5c56be2cd27f7
PH4NTOM <Simonmadsen99@gmail.com>
2024-06-11 13:22:11 +0200

    Added basic socket connection for testing tomorrow

--- client.py
+++ client.py
@@ -1,9 +1,36 @@
 import socket
 
+
+def client_program():
+    # Name and port of the host
+    host = socket.gethostname()
+    port = 5000
+
+    # Get the socket instance and make connection to server
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+
+    # Take input
+    message = input(" -> ")
+
+    while message.lower().strip() != 'bye':
+        # Send message and receive response
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
+
+        # Show in terminal
+        print('Received from server: ' + data)
+
+        # Taking input again
+        message = input(" -> ")
+
+    # Close the connection when receiving 'bye'
+    client_socket.close()
+
+
 def main():
-    print("Hello World :)")
+    client_program()
 
 
 if __name__ == "__main__":
     main()
-
--- server.py
+++ server.py
@@ -1,7 +1,38 @@
 import socket
 
+
+def server_program():
+    # Get hostname and port
+    host = socket.gethostname()
+    port = 5000
+
+    # Get the instance of socket and start listening on host and port.
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+
+    # Max accepting two connections at once.
+    server_socket.listen(2)
+
+    # When connect is accepted, save address and conn in variables
+    conn, address = server_socket.accept()
+
+    print("Connection from: " + str(address))
+
+    while True:
+        # Receive data stream. Won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # If data is not received break
+            break
+        print("from connected client: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())
+
+    conn.close()  # Close the connection
+
+
 def main():
-    print("Hello World")
+    server_program()
 
 
 if __name__ == "__main__":
