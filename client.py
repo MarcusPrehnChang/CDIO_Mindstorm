4c88edf209669ba50916fb0c6226a9b4eaf84466
valde12 <s224289@DTU.dk>
2024-06-12 10:33:29 +0200

    can connect to wifi

--- client.py
+++ client.py
@@ -1,9 +1,52 @@
 import socket
+import time
 
 
-def client_program():
+import pywifi
+from pywifi import const
+import comtypes
+
+def wifi_connect(ssid, password):
+    wifi = pywifi.PyWiFi()
+
+    # Get the first wireless interface.
+    iface = wifi.interfaces()[0]
+
+    # Disconnect all connections.
+    iface.disconnect()
+    time.sleep(1)
+
+    # Create a new profile with the given ssid and password.
+    profile = pywifi.Profile()
+    profile.ssid = ssid
+    profile.auth = const.AUTH_ALG_OPEN
+    profile.akm.append(const.AKM_TYPE_WPA2PSK)
+    profile.cipher = const.CIPHER_TYPE_CCMP
+    profile.key = password
+
+    # Remove all other profiles and add the new one.
+    iface.remove_all_network_profiles()
+    iface.add_network_profile(profile)
+
+    # Connect to the network.
+    iface.connect(profile)
+    time.sleep(10)
+
+    if iface.status() == const.IFACE_CONNECTED:
+        print(f"Connected to {ssid}")
+    else:
+        print("Failed to connect")
+
+    # List available networks.
+    print("Available networks:")
+    networks = iface.scan_results()
+    for network in networks:
+        print(network.ssid)
+
+
+def client_program(hostname):
     # Name and port of the host
-    host = socket.gethostname()
+    host = hostname
     port = 5000
 
     # Get the socket instance and make connection to server
@@ -29,7 +72,8 @@ def client_program():
 
 
 def main():
-    client_program()
+    wifi_connect("OnePlus 11 5G", "s92iynbj")
+    client_program("192.168.23.124")
 
 
 if __name__ == "__main__":
